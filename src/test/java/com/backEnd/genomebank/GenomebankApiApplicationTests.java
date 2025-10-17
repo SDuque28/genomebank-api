a4bb03345fb4e65ff333f9130c8c9f7aafb75365
SDuque28 <santiago.duquer@autonoma.edu.co>
2025-10-17 14:06:07 -0500

    Creation of the proyect

---
+++ src/test/java/com/backEnd/genomebank/GenomebankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backEnd.genomebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenomebankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
